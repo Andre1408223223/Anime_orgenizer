b64bc258e11cca5a396d9525ad906411a7a1e181
Andre140888 <102464@glr.nl>
2025-07-13 12:52:54 +0200

    can now proces episodes and rename them

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ from torrent_orgenizer import get_episode_title_from_filename
 from meddeta import get_metadata_sonnar
 import time
 import json
+import os
 
-""" files = ["[Altair] My Instant Death Ability is So Overpowered S01E10.mkv", "[Altair] My Instant Death Ability is So Overpowered S01E02.mkv"]
+files = ["[Altair] My Instant Death Ability is So Overpowered S01E01.mkv", "[Altair] My Instant Death Ability is So Overpowered S01E02.mkv", "[Altair] My Instant Death Ability is So Overpowered S01E03.mkv"]
 
-data = [] """
+def format_torent_file(files):
+   data = []
 
-
-""" Clean up title """
-""" for file in files:
+   for file in files:
     # use function to clean up the name
     formated_name = get_episode_title_from_filename(file)
+
+    if "[!]" in formated_name:
+     break
 
     # split the title into anime and episde
     anime, episode = formated_name.split("-")
@@ -32,67 +35,92 @@ data = [] """
 
     time.sleep(5)
 
+   data.append({'anime': anime})
+   data.append({'anime': anime, 'season': season,})
 
-data.append({'anime': anime})
-data.append({'anime': anime, 'season': season,})
+   return data
 
-meta = get_metadata_sonnar(data)
+formated_torrents = format_torent_file(files)
 
-print(meta) """
+meta = get_metadata_sonnar(formated_torrents)
 
+def split_meta(meta):
+  shows = []
+  seasons = []
+  episodes = []
+  
+  for entry in meta:
+      genres = entry.get('genres', None)
+      season = entry.get('season', None)
+      episdoe = entry.get('episode', None)
+      
+      if genres:
+          # show
+          entry = {
+              "title": entry.get('title', 'No title'),
+              "description": entry.get('description', 'No description'),
+              "year": entry.get('year', 'Unknown year'),
+              "genres": entry.get('genres', 'Unknown genres'),
+              "rating": entry.get('rating', 'No rating'),
+              "poster_url": entry.get('poster_url', 'No poster URL')
+          }
+          shows.append(entry)
+  
+      elif season is not None and episdoe is not None:
+          # Episdoe
+          entry = {
+              "title": entry.get('title', 'No title'),
+              "episode_number": entry.get('episode', 'N/A'),
+              "description": entry.get('description', 'No description'),
+              "air_date": entry.get('airDate', 'Unknown date')
+          }
+  
+          episodes.append(entry)
+  
+      else:
+          # Season
+          entry = {
+              'season': entry.get('season', 'N/A'),
+              'total_episodes': entry.get('total_episodes', 'N/A')
+          }
+  
+          seasons.append(entry)
 
-""" meta = [{'title': 'A Woman from the Past Appeared. Tomochika is Shocked to Her Core...', 'season': 1, 'episode': 10, 'description': 'Yogiri senses the gaze and murderous intent of someone different. To lure out the culprit, he heads into the city with Tomochika.', 'airDate': '2024-03-08'}, 
-        {'title': 'My Guardian Angel Is So Overpowered, This Other World Is a Piece of Cake!', 'season': 1, 'episode': 2, 'description': 'Yogiri and Tomochika search for their classmates and arrive at a nearby town. Inside the city walls, the two meet a seemingly kind beastkin who has ulterior motives.', 'airDate': '2024-01-12'},
-        {'title': 'My Instant Death Ability Is Overpowered', 'description': "Awaking to absolute chaos and carnage while on a school trip, Yogiri Takatou discovers that everyone in his class has been transported to another world! He had somehow managed to sleep through the entire ordeal himself, missing out on the Gift — powers bestowed upon the others by a mysterious Sage who appeared to transport them. Even worse, he and another classmate were ruthlessly abandoned by their friends, left as bait to distract a nearby dragon. Although not terribly bothered by the thought of dying, he reluctantly decides to protect his lone companion. After all, a lowly Level 1000 monster doesn't stand a chance against his secret power to invoke Instant Death with a single thought! If he can stay awake long enough to bother using it, that is...", 'year': 2024, 'genres': 'Adventure, Animation, Anime, Comedy, Fantasy', 'status': 'ended', 'rating': 6.2, 'poster_url': 'https://artworks.thetvdb.com/banners/v4/series/427883/posters/658b260f5cea0.jpg'}]
- """
+  return shows, seasons, episodes         
 
-meta = [{'title': 'A Woman from the Past Appeared. Tomochika is Shocked to Her Core...', 'season': 1, 'episode': 10, 'description': 'Yogiri senses the gaze and murderous intent of someone different. To lure out the culprit, he heads into the city with Tomochika.', 'airDate': '2024-03-08'},
-        {'title': 'My Guardian Angel Is So Overpowered, This Other World Is a Piece of Cake!', 'season': 1, 'episode': 2, 'description': 'Yogiri and Tomochika search for their classmates and arrive at a nearby town. Inside the city walls, the two meet a seemingly kind beastkin who has ulterior motives.', 'airDate': '2024-01-12'},
-        {'title': 'My Instant Death Ability Is Overpowered', 'description': "Awaking to absolute chaos and carnage while on a school trip, Yogiri Takatou discovers that everyone in his class has been transported to another world! He had somehow managed to sleep through the entire ordeal himself, missing out on the Gift — powers bestowed upon the others by a mysterious Sage who appeared to transport them. Even worse, he and another classmate were ruthlessly abandoned by their friends, left as bait to distract a nearby dragon. Although not terribly bothered by the thought of dying, he reluctantly decides to protect his lone companion. After all, a lowly Level 1000 monster doesn't stand a chance against his secret power to invoke Instant Death with a single thought! If he can stay awake long enough to bother using it, that is...", 'year': 2024, 'genres': 'Adventure, Animation, Anime, Comedy, Fantasy', 'status': 'ended', 'rating': 6.2, 'poster_url': 'https://artworks.thetvdb.com/banners/v4/series/427883/posters/658b260f5cea0.jpg'},
-        {'season': 1, 'total_episodes': 12}]
-        
+shows ,seasons, episodes = split_meta(meta)
 
-        
-shows = []
-seasons = []
-episodes = []
-
-for entry in meta:
-    genres = entry.get('genres', None)
-    season = entry.get('season', None)
-    episdoe = entry.get('episode', None)
-    
-    if genres:
-        # show
-        entry = {
-            "title": entry.get('title', 'No title'),
-            "description": entry.get('description', 'No description'),
-            "year": entry.get('year', 'Unknown year'),
-            "genres": entry.get('genres', 'Unknown genres'),
-            "rating": entry.get('rating', 'No rating'),
-            "poster_url": entry.get('poster_url', 'No poster URL')
-        }
-        shows.append(entry)
-
-    elif season is not None and episdoe is not None:
-        # Episdoe
-        entry = {
-            "title": entry.get('title', 'No title'),
-            "episode_number": entry.get('episode', 'N/A'),
-            "description": entry.get('description', 'No description'),
-            "air_date": entry.get('airDate', 'Unknown date')
-        }
-
-        episodes.append(entry)
-
-    else:
-        # Season
-        entry = {
-            'season': entry.get('season', 'N/A'),
-            'total_episodes': entry.get('total_episodes', 'N/A')
-        }
-
-        seasons.append(entry)
+show_title = shows[0]['title']
+season_nm = seasons[0]['season']
 
 
-print(seasons)
+
+show_folder_path = show_title
+season_folder_path = os.path.join(show_folder_path, f"Season {str(season_nm)}")
+
+
+os.makedirs(show_title, exist_ok=True)
+os.makedirs(season_folder_path, exist_ok=True)
+
+for episode in episodes:
+   episode_title = episode['title']
+   episode_nm = episode['episode_number']
+
+   orignial_file = files[int(episode_nm) -1]
+
+   _, ext = os.path.splitext(orignial_file)
+
+   formatted_title = f"{show_title} - S{int(season_nm):02}E{int(episode_nm):02} - {episode_title}{ext}"
+
+   os.rename(orignial_file, os.path.join(season_folder_path, formatted_title))
+
+
+
+""" print("\n=== Shows ===")
+print(json.dumps(shows, indent=4, ensure_ascii=False))
+
+print("\n=== Seasons ===")
+print(json.dumps(seasons, indent=4, ensure_ascii=False))
+
+print("\n=== Episodes ===")
+print(json.dumps(episodes, indent=4, ensure_ascii=False)) """
